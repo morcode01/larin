8ed08d5140acabf8313d69164b83ce85b1731f58
Rui Moreira <rui@morcode.pt>
2021-05-11 13:49:39 +0100

    image path

--- src/Main/Home.js
+++ src/Main/Home.js
@@ -7,10 +7,11 @@ class Home extends React.Component {
 		return(
 			<div className="home main">
 				<Carousel>
+				
 				    <Carousel.Item>
 						<img
 						  className="d-block w-100"
-						  src={'/Slides/slide-1.jpg'}
+						  src={![](/Slides/slide-1.jpg)}
 						  alt="First slide"
 						/>
 						<Carousel.Caption>
@@ -21,7 +22,7 @@ class Home extends React.Component {
 					<Carousel.Item>
 						<img
 						  className="d-block w-100"
-						  src={'/Slides/slide-2.jpg'}
+						  src={![](/Slides/slide-2.jpg)}
 						  alt="Second slide"
 						/>
 						<Carousel.Caption>
@@ -32,7 +33,7 @@ class Home extends React.Component {
 					<Carousel.Item>
 						<img
 						  className="d-block w-100"
-						  src={'/Slides/slide-3.jpg'}
+						  src={![](/Slides/slide-3.jpg}
 						  alt="Third slide"
 						/>
 						<Carousel.Caption>
