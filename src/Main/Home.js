6c8862b53cac97f8f92485c6d0e47e7c4a5e25db
Rui Moreira <rui@morcode.pt>
2021-06-14 09:38:51 +0100

    home update

--- src/Main/Home.js
+++ src/Main/Home.js
@@ -3,11 +3,83 @@ import './Home.css';
 import Carousel from 'react-bootstrap/Carousel';
 import Col from 'react-bootstrap/Col';
 import Row from 'react-bootstrap/Row';
+import Card from 'react-bootstrap/Card';
+import Button from 'react-bootstrap/Button';
+import Accordion from 'react-bootstrap/Accordion';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import * as Icons from '@fortawesome/free-solid-svg-icons';
+import Select from 'react-select'
+import logoIcon from '../Img/larin-icon.svg';
 
 class Home extends React.Component {
+	constructor(){
+    super();
+		this.state = {
+		   arrowRotation1: 'arrow-up',
+		   arrowRotation2: 'arrow-down',
+		   arrowRotation3: 'arrow-down'
+		}
+		this.rotateArrow = this.rotateArrow.bind(this);
+	}
+	  
+	
+	rotateArrow(i){
+		if(i==1){
+			if(this.state.arrowRotation1 == 'arrow-down'){
+				this.setState({
+				  arrowRotation1: 'arrow-up',
+				  arrowRotation2: 'arrow-down',
+				  arrowRotation3: 'arrow-down'
+				});
+			}
+			else{
+				this.setState({
+				  arrowRotation1: 'arrow-down',
+				  arrowRotation2: 'arrow-down',
+				  arrowRotation3: 'arrow-down'
+				});
+			}
+		}
+		if(i==2){
+			if(this.state.arrowRotation2 == 'arrow-down'){
+				this.setState({
+				   arrowRotation1: 'arrow-down',
+				   arrowRotation2: 'arrow-up',
+				   arrowRotation3: 'arrow-down'
+				});
+			}
+			else{
+				this.setState({
+				  arrowRotation1: 'arrow-down',
+				  arrowRotation2: 'arrow-down',
+				  arrowRotation3: 'arrow-down'
+				});
+			}
+		}
+		if(i==3){
+			if(this.state.arrowRotation3 == 'arrow-down'){
+				this.setState({
+				  arrowRotation1: 'arrow-down',
+				  arrowRotation2: 'arrow-down',
+				  arrowRotation3: 'arrow-up'
+				});
+			}
+			else{
+				this.setState({
+				  arrowRotation1: 'arrow-down',
+				  arrowRotation2: 'arrow-down',
+				  arrowRotation3: 'arrow-down'
+				});
+			}
+		}
+	}
 	render () {
+		const options = [
+		  { value: 'pt', label: 'Português' },
+		  { value: 'en', label: 'English' },
+		  { value: 'en', label: 'Español' }
+		]
+		const { arrowRotation1, arrowRotation2, arrowRotation3 } = this.state;
 		return(
 			<div className="home main">
 				<div className="carousel-container">
@@ -78,9 +150,9 @@ class Home extends React.Component {
 						<p>Coloque o seu espaço disponível para todos que procuram um lar</p>
 					</div>
 				</div>
-				<div className="features">
-					<h3>Melhores lares e residências no Porto</h3>
-					<p>Encontre o lar ou residência para proporcionar o maior conforto ao seu ente mais querido.</p>
+				<div className="features section">
+					<h3 className="title-default">Melhores lares e residências no Porto</h3>
+					<p className="subtitle-default">Encontre o lar ou residência para proporcionar o maior conforto ao seu ente mais querido.</p>
 					<Row>
 						<Col xs={6}>
 							<img
@@ -127,6 +199,208 @@ class Home extends React.Component {
 							<p className="house-rating"><FontAwesomeIcon icon={Icons.faStar} /> 4.5 <span>(888)</span></p>
 						</Col>
 					</Row>
+					<a href="#" className="btn-view-more">Ver todos <FontAwesomeIcon icon={Icons.faArrowRight} /></a>
+				</div>
+				<div className="explore-zone section">
+					<h3 className="title-default">Explore por zonas</h3>
+					<p className="subtitle-default">Uma nova seleção de casas com conforto e qualidade verificados</p>
+					<div className="zones-scroll">
+						<div className="zones-container">
+							<div className="zone-item">
+								<div className="zone-img-container">
+									<img
+									  className="d-block w-100 zone-img"
+									  src={process.env.PUBLIC_URL + '/Slides/slide-1.jpg'}
+									  alt="First slide"
+									/>
+									<p>Lares no Porto</p>
+								</div>
+								<p className="zone-district">Distrito Porto</p>
+								<p className="zone-desc">Veja a oferta de lares, centros de…</p>
+							</div>
+							<div className="zone-item">
+								<div className="zone-img-container">
+									<img
+									  className="d-block w-100 zone-img"
+									  src={process.env.PUBLIC_URL + '/Slides/slide-1.jpg'}
+									  alt="First slide"
+									/>
+									<p>Lares em Portalegre</p>
+								</div>
+								<p className="zone-district">Distrito Portalegre</p>
+								<p className="zone-desc">Veja a oferta no distrito de Portalegre</p>
+							</div>
+							<div className="zone-item">
+								<div className="zone-img-container">
+									<img
+									  className="d-block w-100 zone-img"
+									  src={process.env.PUBLIC_URL + '/Slides/slide-1.jpg'}
+									  alt="First slide"
+									/>
+									<p>Lares no Porto</p>
+								</div>
+								<p className="zone-district">Distrito Porto</p>
+								<p className="zone-desc">Veja a oferta de lares, centros de…</p>
+							</div>
+							<div className="zone-item">
+								<div className="zone-img-container">
+									<img
+									  className="d-block w-100 zone-img"
+									  src={process.env.PUBLIC_URL + '/Slides/slide-1.jpg'}
+									  alt="First slide"
+									/>
+									<p>Lares em Portalegre</p>
+								</div>
+								<p className="zone-district">Distrito Portalegre</p>
+								<p className="zone-desc">Veja a oferta no distrito de Portalegre</p>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div className="add-home section">
+					<img
+					  className="d-block w-100 add-home-img"
+					  src={process.env.PUBLIC_URL + '/Slides/slide-1.jpg'}
+					  alt="First slide"
+					/>
+					<div className="add-home-container">
+						<h3>Saiba quanto poderia ganhar ao promover o seu espaço</h3>
+						<p>Coloque o seu espaço disponível para todos que procuram um lar</p>
+						<a href="#" className="btn-white">Adicionar um lar</a>
+					</div>
+				</div>
+				<div className="services section">
+					<h3 className="title-default">Serviços na 3ª pessoa</h3>
+					<p className="subtitle-default">Encontre os melhores serviços para proporcionar o maior conforto ao seu ente mais querido.</p>
+					<div className="services-scroll">
+						<div className="services-container">
+							<div className="service-item">
+								<img
+								  className="d-block w-100 service-img"
+								  src={process.env.PUBLIC_URL + '/Slides/slide-1.jpg'}
+								  alt="First slide"
+								/>
+								<p className="service-location">Mafra, Lisboa</p>
+								<p className="service-name">Dia de descanso nas termas S. José Salvador</p>
+								<p className="service-price">Desde 1.200 €/ mês</p>
+								<p className="service-rating"><FontAwesomeIcon icon={Icons.faStar} /> 4.5 <span>(888)</span></p>
+							</div>
+							<div className="service-item">
+								<img
+								  className="d-block w-100 service-img"
+								  src={process.env.PUBLIC_URL + '/Slides/slide-1.jpg'}
+								  alt="First slide"
+								/>
+								<p className="service-location">Mafra, Lisboa</p>
+								<p className="service-name">Dia de descanso nas termas S. José Salvador</p>
+								<p className="service-price">Desde 1.200 €/ mês</p>
+								<p className="service-rating"><FontAwesomeIcon icon={Icons.faStar} /> 4.5 <span>(888)</span></p>
+							</div>
+							<div className="service-item">
+								<img
+								  className="d-block w-100 service-img"
+								  src={process.env.PUBLIC_URL + '/Slides/slide-1.jpg'}
+								  alt="First slide"
+								/>
+								<p className="service-location">Mafra, Lisboa</p>
+								<p className="service-name">Dia de descanso nas termas S. José Salvador</p>
+								<p className="service-price">Desde 1.200 €/ mês</p>
+								<p className="service-rating"><FontAwesomeIcon icon={Icons.faStar} /> 4.5 <span>(888)</span></p>
+							</div>
+							<div className="service-item">
+								<img
+								  className="d-block w-100 service-img"
+								  src={process.env.PUBLIC_URL + '/Slides/slide-1.jpg'}
+								  alt="First slide"
+								/>
+								<p className="service-location">Mafra, Lisboa</p>
+								<p className="service-name">Dia de descanso nas termas S. José Salvador</p>
+								<p className="service-price">Desde 1.200 €/ mês</p>
+								<p className="service-rating"><FontAwesomeIcon icon={Icons.faStar} /> 4.5 <span>(888)</span></p>
+							</div>
+						</div>
+					</div>
+					<a href="#" className="btn-view-more">Ver todos <FontAwesomeIcon icon={Icons.faArrowRight} /></a>
+				</div>
+				<div className="extra section">
+					<h2 className="title-default">A casa onde o seu pai, avós ou bisavós irão morar é importante!</h2>
+					<p className="subtitle-default">Procure as melhores condições para aqueles de quem gostamos mais, de modo que possam facilmente encontrar as condições necessárias para continuar a sorrir, cheio de conforto que tanto merecem.<br/><b>Bem vindo a casa!</b></p>
+				</div>
+				<div className="footer section">
+					<Select 
+					defaultValue={options[0]}
+					options={options} 
+					/>
+					<Accordion defaultActiveKey="0">
+					    <Card>
+							<Card.Header>
+								<Accordion.Toggle as={Button} onClick={() => this.rotateArrow(1)} variant="link" eventKey="0">
+									Larin
+									<div className={'arrow-div ' + arrowRotation1} >
+									<FontAwesomeIcon icon={Icons.faAngleDown} />
+									</div>
+								</Accordion.Toggle>
+							</Card.Header>
+							<Accordion.Collapse eventKey="0">
+								<Card.Body>
+									<ul>
+										<li>Sobre o Larin</li>
+										<li>Testemunhos</li>
+										<li>Dicas</li>
+										<li>Blog</li>
+										<li>Centro de imprensa</li>
+										<li>Contacte-nos</li>
+									</ul>
+								</Card.Body>
+							</Accordion.Collapse>
+						</Card>
+						<Card>
+							<Card.Header>
+								<Accordion.Toggle as={Button} onClick={() => this.rotateArrow(2)} variant="link" eventKey="1">
+									Precisa de ajuda
+									<div className={'arrow-div ' + arrowRotation2} >
+									<FontAwesomeIcon icon={Icons.faAngleDown} />
+									</div>
+								</Accordion.Toggle>
+							</Card.Header>
+							<Accordion.Collapse eventKey="1">
+								<Card.Body>
+									<ul>
+										<li>Sobre o Larin</li>
+										<li>Testemunhos</li>
+										<li>Dicas</li>
+										<li>Blog</li>
+										<li>Centro de imprensa</li>
+										<li>Contacte-nos</li>
+									</ul>
+								</Card.Body>
+							</Accordion.Collapse>
+						</Card>
+						<Card>
+							<Card.Header>
+								<Accordion.Toggle as={Button} onClick={() => this.rotateArrow(3)} variant="link" eventKey="2">
+									Adicionar o seu espaço
+									<div className={'arrow-div ' + arrowRotation3} >
+									<FontAwesomeIcon icon={Icons.faAngleDown} />
+									</div>
+								</Accordion.Toggle>
+							</Card.Header>
+							<Accordion.Collapse eventKey="2">
+								<Card.Body>
+									<ul>
+										<li>Sobre o Larin</li>
+										<li>Testemunhos</li>
+										<li>Dicas</li>
+										<li>Blog</li>
+										<li>Centro de imprensa</li>
+										<li>Contacte-nos</li>
+									</ul>
+								</Card.Body>
+							</Accordion.Collapse>
+						</Card>
+					</Accordion>
+					<p className="footer-links"><a href="#">Termos e condições</a> • <a href="#">Privacidade</a> • <a href="#">Site map</a></p>
+					<img src={logoIcon} className="logo-icon" />
 				</div>
 			</div>
 		)
